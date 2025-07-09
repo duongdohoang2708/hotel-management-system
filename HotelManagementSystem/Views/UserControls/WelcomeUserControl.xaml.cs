8284f3c25098fd1fb7f40101cdd7b0400e205ede
Duong Do <dohoangduong2708@gmail.com>
2025-07-09 11:47:12 +0700

    Add WelcomeUserControl for user greeting interface with background image and welcome text. Implement associated code-behind for initialization.

---
+++ HotelManagementSystem/Views/UserControls/WelcomeUserControl.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace HotelManagementSystem.Views.UserControls
+{
+    public partial class WelcomeUserControl : UserControl
+    {
+        public WelcomeUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
